66d7bc6240c7982a6e493e347e32343873d0b801
marshal god <nontawatjss@gmail.com>
2023-07-19 19:26:22 +0700

    setup project

---
+++ example-currency-of-btcTests/example_currency_of_btcTests.swift
@@ -0,0 +1,36 @@
+//
+//  example_currency_of_btcTests.swift
+//  example-currency-of-btcTests
+//
+//  Created by Nontawat Kanboon on 19/7/2566 BE.
+//
+
+import XCTest
+@testable import example_currency_of_btc
+
+final class example_currency_of_btcTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
